37f3c4fca60db82c019811fd2f79257c4de42518
Gabriele Giunchi <gabriele.giunchi1994@gmail.com>
2018-06-12 11:42:16 +0200

    implemented first version of Pearce algorithm

--- SCC/SCCPearce.cpp
+++ SCC/SCCPearce.cpp
@@ -2,10 +2,62 @@
 
 SCCPearce::SCCPearce() :SCCStrategy("Pearce") { }
 
+void SCCPearce::SCCPearceUtil(Graph* g, int v, int rindex[], stack<int>* stack, boost::dynamic_bitset<>* inComponent,
+	boost::dynamic_bitset<>* visited, SCCList* strongComponents) {
+
+	static int index = 0;
+	visited->set(v, true);
+	bool root = true;
+	inComponent->set(v, false);
+	rindex[v] = index;
+	index++;
+	
+	auto children = g->getChildren(v);
+	for (auto it = children.begin(); it != children.end(); ++it) {
+		int w = *it;
+		if (!(*visited)[w]) {
+			this->SCCPearceUtil(g, w, rindex, stack, inComponent, visited, strongComponents);
+		}
+
+		if (!(*inComponent)[w] && rindex[w] < rindex[v]) {
+			rindex[v] = rindex[w];
+			root = false;
+		}
+	}
+
+	if (root) {
+		inComponent->set(v, true);
+		StronglyConnectedComponent component;
+		while (!stack->empty() && rindex[v] <= rindex[stack->top()]) {
+			int w = stack->top();
+			stack->pop();
+			inComponent->set(w, true);
+			component.addNode(w);
+		}
+		component.addNode(v);
+		strongComponents->addComponent(component);
+	}
+	else {
+		stack->push(v);
+	}
+}
+
 SCCList SCCPearce::getSCC(Graph* g) {
 	if (g->getSize() <= 0) {
 		return SCCList();
 	}
 
-	return SCCList();
+	int *rindex = new int[g->getSize()];
+	boost::dynamic_bitset<>* visited = new boost::dynamic_bitset<>(g->getSize());
+	boost::dynamic_bitset<>* inComponent = new boost::dynamic_bitset<>(g->getSize());
+	stack<int> *stack = new std::stack<int>();
+	SCCList strongComponents;
+
+	for (int i = 0; i < g->getSize(); i++) {
+		if (!(*visited)[i]) {
+			this->SCCPearceUtil(g, i, rindex, stack, inComponent, visited, &strongComponents);
+		}
+	}
+
+	return strongComponents;
 }
--- SCC/SCCPearce.h
+++ SCC/SCCPearce.h
@@ -1,6 +1,10 @@
 #pragma once
 #include "SCCStrategy.h"
+
 class SCCPearce : public SCCStrategy {
+private:
+	void SCCPearceUtil(Graph* g, int v, int rindex[], stack<int>* stack, boost::dynamic_bitset<>* inComponent, 
+		boost::dynamic_bitset<>* visited, SCCList* strongComponents);
 public:
 	SCCPearce();
 	SCCList getSCC(Graph* g);
