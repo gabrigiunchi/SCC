d48e617e97bb56956827d945c1827ae48477d5eb
Gabriele Giunchi <gabriele.giunchi1994@gmail.com>
2018-06-22 21:50:13 +0200

    change input type from maxSize to number of tests

--- SCC/main.cpp
+++ SCC/main.cpp
@@ -7,15 +7,17 @@
 #include "SCCPearce.h"
 #include "utils.h"
 
+#define DEFAULT_N_TESTS 10
+
 using namespace std;
 
-void getInput(int* minSize, int *maxSize, int* step) {
-	cout << " Insert <min graph size> <max graph size> <increment>" << endl << " > ";
-	cin >> *minSize >> *maxSize >> *step;
+void getInput(int* n, int* minSize, int* step) {
+	cout << " Insert <number of tests> <start graph size> <increment>" << endl << " > ";
+	cin >> *n >> *minSize >> *step;
 
 	// Handle invalid input
 	if (*minSize < 0) *minSize = 0;
-	if (*maxSize < 0) *maxSize = 0;
+	if (*n < 0) *n = DEFAULT_N_TESTS;
 	if (*step < 0) *step = 0;
 }
 
@@ -29,47 +31,49 @@ int parseInt(char digit) {
 }
 
 void menu() {
-	int minSize = 1000;
-	int maxSize = 10000;
-	int step = 1000;
-	int n = step != 0 ? (maxSize - minSize) / step + 1 : 0; // Set number of tests accordingly to parameters
+	int minSize = 100;
+	int step = 0;
+	int n = DEFAULT_N_TESTS;
 	string input;
+	bool exit = false;
 
 	do {
-		cout << endl << " 1) Test Tarjan algorithm" << endl
+		cout << endl 
+			<< " 0) Exit" << endl
+			<< " 1) Test Tarjan algorithm" << endl
 			<< " 2) Test Nuutila algorithm" << endl
 			<< " 3) Test Pearce algorithm" << endl
 			<< " 4) Test all algorithms" << endl
 			<< " 5) Memory test" << endl
-			<< " 6) Set parameters (current: minSize=" << minSize << ", maxSize=" << maxSize << ", step=" << step << ")"
-			<< endl << " > ";
+			<< " 6) Set parameters (current: number of tests=" << n << ", start graph size=" << minSize << ", step=" << step << ")"
+			<< endl << endl << " > ";
 
 		cin >> input;
 		int code = parseInt(input[0]); // Parse the first character of the string
 
 		cout << endl;
 		switch (code) {
-		case 1: cout << Tester(new SCCTarjan()).performeTests(n, minSize, step).toString() << endl; break;
-		case 2: cout << Tester(new SCCNuutila()).performeTests(n, minSize, step).toString() << endl; break;
-		case 3: cout << Tester(new SCCPearce()).performeTests(n, minSize, step).toString() << endl; break;
-		case 4: {
-			BenchmarkManager resultTarjan = Tester(new SCCTarjan()).performeTests(n, minSize, step);
-			BenchmarkManager resultNuutila = Tester(new SCCNuutila()).performeTests(n, minSize, step);
-			BenchmarkManager resultPearce = Tester(new SCCPearce()).performeTests(n, minSize, step);
-			cout << " Tarjan: " << resultTarjan.toString() << endl
-				<< " Nuutila: " << resultNuutila.toString() << endl
-				<< " Pearce: " << resultPearce.toString() << endl;
-			break;
+			case 0: exit = true; break;
+			case 1: cout << Tester(new SCCTarjan()).performeTests(n, minSize, step).toString() << endl; break;
+			case 2: cout << Tester(new SCCNuutila()).performeTests(n, minSize, step).toString() << endl; break;
+			case 3: cout << Tester(new SCCPearce()).performeTests(n, minSize, step).toString() << endl; break;
+			case 4: {
+				BenchmarkManager resultTarjan = Tester(new SCCTarjan()).performeTests(n, minSize, step);
+				BenchmarkManager resultNuutila = Tester(new SCCNuutila()).performeTests(n, minSize, step);
+				BenchmarkManager resultPearce = Tester(new SCCPearce()).performeTests(n, minSize, step);
+				cout << " Tarjan: " << resultTarjan.toString() << endl
+					<< " Nuutila: " << resultNuutila.toString() << endl
+					<< " Pearce: " << resultPearce.toString() << endl;
+				break;
+			}
+			case 5: Tester(new SCCPearce()).memoryTest(); break;
+			case 6: {
+				getInput(&n, &minSize, &step);
+				break;
+			}
+			default: break;
 		}
-		case 5: Tester(new SCCPearce()).memoryTest(); break;
-		case 6: {
-			getInput(&minSize, &maxSize, &step);
-			n = step != 0 ? (maxSize - minSize) / step + 1 : 0; // Update number of tests
-			break;
-		}
-		default: break;
-		}
-	} while (input != "exit" && input != "quit");
+	} while (!exit);
 }
 
 int main(int argc, char* argv[]) {
