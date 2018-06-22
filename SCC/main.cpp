15ebd160e4ef0249c6c17fdb1203e3d98787e239
Gabriele Giunchi <gabriele.giunchi1994@gmail.com>
2018-06-22 13:32:16 +0200

    minor changes in input handling
    parse user input and check if it's valid, add menu option for setting parameters

--- SCC/main.cpp
+++ SCC/main.cpp
@@ -9,64 +9,66 @@
 
 using namespace std;
 
-void getInput(int* n, int *minSize, int* step) {
-	cout << " Do you want to customize the test parameters? (y/n) " << endl << " > ";
-	string input;
-	cin >> input;
+void getInput(int* minSize, int *maxSize, int* step) {
+	cout << " Insert <min graph size> <max graph size> <increment>" << endl << " > ";
+	cin >> *minSize >> *maxSize >> *step;
 
-	if (input == "y") {
-		cout << " Insert <number of tests> <start graph size> <increment>" << endl << " > ";
-		cin >> *n >> *minSize >> *step;
-	}
-
-	cout << endl;
+	// Handle invalid input
+	if (*minSize < 0) *minSize = 0;
+	if (*maxSize < 0) *maxSize = 0;
+	if (*step < 0) *step = 0;
 }
 
-void menu(int minSize, int step, int maxSize) {
-	string input;
-	
-	do {
-		int n = (maxSize - minSize) / step + 1;
+int parseInt(char digit) {
+	// Handle invalid input
+	if (digit < '0' && digit > '9') {
+		return 0;
+	}
 
+	return digit - '0';
+}
+
+void menu() {
+	int minSize = 1000;
+	int maxSize = 10000;
+	int step = 1000;
+	int n = step != 0 ? (maxSize - minSize) / step + 1 : 0; // Set number of tests accordingly to parameters
+	string input;
+
+	do {
 		cout << endl << " 1) Test Tarjan algorithm" << endl
 			<< " 2) Test Nuutila algorithm" << endl
 			<< " 3) Test Pearce algorithm" << endl
 			<< " 4) Test all algorithms" << endl
 			<< " 5) Memory test" << endl
+			<< " 6) Set parameters (current: minSize=" << minSize << ", maxSize=" << maxSize << ", step=" << step << ")"
 			<< endl << " > ";
 
 		cin >> input;
+		int code = parseInt(input[0]); // Parse the first character of the string
 
-		if (input == "1") {
-			cout << endl;
-			//getInput(&n, &minSize, &step);
-			cout << Tester(new SCCTarjan()).performeTests(n, minSize, step).toString() << endl;
-		}
-		else if (input == "2") {
-			cout << endl;
-			//getInput(&n, &minSize, &step);
-			cout << Tester(new SCCNuutila()).performeTests(n, minSize, step).toString() << endl;
-		}
-		else if (input == "3") {
-			cout << endl;
-			//getInput(&n, &minSize, &step);
-			cout << Tester(new SCCPearce()).performeTests(n, minSize, step).toString() << endl;
-		}
-		else if (input == "4") {
-			cout << endl;
-			//getInput(&n, &minSize, &step);
+		cout << endl;
+		switch (code) {
+		case 1: cout << Tester(new SCCTarjan()).performeTests(n, minSize, step).toString() << endl; break;
+		case 2: cout << Tester(new SCCNuutila()).performeTests(n, minSize, step).toString() << endl; break;
+		case 3: cout << Tester(new SCCPearce()).performeTests(n, minSize, step).toString() << endl; break;
+		case 4: {
 			BenchmarkManager resultTarjan = Tester(new SCCTarjan()).performeTests(n, minSize, step);
 			BenchmarkManager resultNuutila = Tester(new SCCNuutila()).performeTests(n, minSize, step);
 			BenchmarkManager resultPearce = Tester(new SCCPearce()).performeTests(n, minSize, step);
 			cout << " Tarjan: " << resultTarjan.toString() << endl
 				<< " Nuutila: " << resultNuutila.toString() << endl
 				<< " Pearce: " << resultPearce.toString() << endl;
+			break;
 		}
-		else if (input == "5") {
-			cout << endl;
-			Tester(new SCCNuutila()).memoryTest();
+		case 5: Tester(new SCCPearce()).memoryTest(); break;
+		case 6: {
+			getInput(&minSize, &maxSize, &step);
+			n = step != 0 ? (maxSize - minSize) / step + 1 : 0; // Update number of tests
+			break;
 		}
-
+		default: break;
+		}
 	} while (input != "exit" && input != "quit");
 }
 
@@ -74,15 +76,7 @@ int main(int argc, char* argv[]) {
 	// Inizialize seed for the random fuction
 	srand(time(NULL));
 
-	int minSize = 1000;
-	int step = 1000;
-	int maxSize = 10000;
-
-	if (argc >= 2) minSize = atoi(argv[1]);
-	if (argc >= 3) step = atoi(argv[2]);
-	if (argc > 3) maxSize = atoi(argv[3]);
-
-	menu(minSize, step, maxSize);
+	menu();
 
 	return 0;
 }
